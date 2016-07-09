4ebd3a9c623868f35b2e6a01a8a00bffac63a6dc
Admin <admin@A51-01.local>
2016-07-08 21:57:05 -0500

    correct port number

--- server.js
+++ server.js
@@ -7,5 +7,7 @@ app.get('/', function(request, response){
   response.sendFile(path.join(__dirname) + '/index.html');
 });
 
+app.set('port', (process.env.PORT || 5000));
 app.listen(1337);
+
 console.log('Here we go!');
