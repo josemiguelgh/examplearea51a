d17983533c5c432e54f010d43166aaf1db65df9a
Admin <admin@A51-01.local>
2016-07-08 21:47:34 -0500

    include express and first page to run

---
+++ server.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var app = express();
+//nos sirve para definir las rutas que voy a trabajar en mi aplicacion
+var path = require('path')
+
+app.get('/', function(request, response){
+  response.sendFile(path.join(__dirname) + '/index.html');
+});
+
+app.listen(1337);
+console.log('Here we go!');
