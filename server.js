845e77bc6838a2ca8cfa0c61981c8f17e026acf3
Admin <admin@A51-01.local>
2016-07-11 20:24:22 -0500

    adding several routes to the api

--- server.js
+++ server.js
@@ -2,10 +2,47 @@ var express = require('express');
 var app = express();
 //nos sirve para definir las rutas que voy a trabajar en mi aplicacion
 var path = require('path')
+var adminRouter = express.Router();
+//var externalRouter = express.Router();
+//var publicRouter = express.Router();
 
 app.get('/', function(request, response){
   response.sendFile(path.join(__dirname) + '/index.html');
 });
+
+//Middleware
+adminRouter.use(function (req, res, next){
+  console.log('------->', req.method, req.url);
+  next();
+});
+
+adminRouter.param('name', function (req, res, next, name){
+  console.log('req.name: ', req.name);
+  console.log('name: ', name);
+  req.name = 'Mr. robot wa here!';
+  next();
+});
+
+//Rutas
+adminRouter.get('/', function(req, res){
+  res.send('Estoy en la pagina principal del admin');
+});
+
+adminRouter.get('/users', function(req, res){
+  console.log('Ya llegue a la vista de usuarios');
+  res.send('Aqui se mostraran los usuarios');
+});
+
+adminRouter.get('/users/:name', function(req, res){
+  //res.send('Hola ' + req.params.name);
+  res.send('Hola ' + req.name);
+});
+
+adminRouter.get('/posts', function(req, res){
+  res.send('Aqui se mostraran los articulos');
+});
+
+app.use('/admin', adminRouter);
 
 app.set('port', (process.env.PORT || 5000));
 app.listen(app.get('port'));
