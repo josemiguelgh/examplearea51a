e96ef9433bfc78e33e9304385f0e1bfc6f62bb30
Admin <admin@A51-01.local>
2016-07-11 21:36:29 -0500

    include login middleware

--- server.js
+++ server.js
@@ -3,19 +3,20 @@ var app = express();
 //nos sirve para definir las rutas que voy a trabajar en mi aplicacion
 var path = require('path')
 var adminRouter = express.Router();
-//var externalRouter = express.Router();
+var externalRouter = express.Router();
 //var publicRouter = express.Router();
 
-app.get('/', function(request, response){
+app.get('/', function(request, responsse){
   response.sendFile(path.join(__dirname) + '/index.html');
 });
 
 //Middleware
+//mas generico, para toda la aplicacion
 adminRouter.use(function (req, res, next){
   console.log('------->', req.method, req.url);
   next();
 });
-
+//estos trabajan a nivel de parametro
 adminRouter.param('name', function (req, res, next, name){
   console.log('req.name: ', req.name);
   console.log('name: ', name);
@@ -23,7 +24,26 @@ adminRouter.param('name', function (req, res, next, name){
   next();
 });
 
+externalRouter.param('username', function(req, res, next, username){
+  if(username === 'mrrobot'){
+    req.username = 'jose';
+    next();
+  }else{
+    res.redirect('/external/error');
+  }
+});
+
+externalRouter.param('password', function(req, res, next, password){
+
+  if(password === '123456'){
+    next();
+  }else{
+    res.redirect('/external/error');
+  }
+});
+
 //Rutas
+//adminRouter
 adminRouter.get('/', function(req, res){
   res.send('Estoy en la pagina principal del admin');
 });
@@ -42,7 +62,18 @@ adminRouter.get('/posts', function(req, res){
   res.send('Aqui se mostraran los articulos');
 });
 
+
+//externalRouter
+externalRouter.get('/login/:username/:password', function(req, res){
+    res.send('Congratulations ' + req.username + ', you entered!!');
+});
+
+externalRouter.get('/error', function(req, res){
+  res.send('You are not allowed to enter this site. Get out!!')
+});
+
 app.use('/admin', adminRouter);
+app.use('/external', externalRouter);
 
 app.set('port', (process.env.PORT || 5000));
 app.listen(app.get('port'));
