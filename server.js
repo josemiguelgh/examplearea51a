b95380c15757abc952ca5027db9d0ef0d2ed52f2
Admin <admin@A51-01.local>
2016-07-15 20:00:03 -0500

    refactor code

--- server.js
+++ server.js
@@ -1,3 +1,6 @@
+//call the packages
+
+var mongoose = require('mongoose');
 var express = require('express');
 var app = express();
 //nos sirve para definir las rutas que voy a trabajar en mi aplicacion
@@ -5,100 +8,153 @@ var path = require('path')
 var adminRouter = express.Router();
 var externalRouter = express.Router();
 //var publicRouter = express.Router();
+var bodyParser = require('body-parser');
+var morgan = require('morgan');
 
-app.get('/', function(request, responsse){
-  response.sendFile(path.join(__dirname) + '/index.html');
-});
+var port = process.env.PORT || 5000;
 
-//Middleware
-//mas generico, para toda la aplicacion
-adminRouter.use(function (req, res, next){
-  console.log('------->', req.method, req.url);
+
+//APP CONFIGURATION
+app.use(bodyParser.urlencoded({ extended:true }));
+app.use(bodyParser.json());
+
+//CORS
+app.use(function(req, res, next){
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-Width, content-type, Authorization');
   next();
 });
-//estos trabajan a nivel de parametro
-adminRouter.param('name', function (req, res, next, name){
-  console.log('req.name: ', req.name);
-  console.log('name: ', name);
-  req.name = 'Mr. robot wa here!';
-  next();
+
+//
+app.use(morgan('dev'));
+
+mongoose.connect('mongodb://localhost/pokemon');
+
+//API ROUTES
+//Main/basic route
+app.get('/', function(req, res){
+  res.send('Welcome to the real world');
 });
 
-externalRouter.param('username', function(req, res, next, username){
-  if(username === 'mrrobot'){
-    req.username = 'jose';
-    next();
-  }else{
-    res.redirect('/external/error');
-  }
+//Express router instance
+var apiRouter = express.Router();
+
+//Accesed at GET http://localhost:5000/api
+apiRouter.get('/', function(req, res){
+  res.json({ message: 'Stop to try hit me and hit me!' });
 });
 
-externalRouter.param('password', function(req, res, next, password){
+//Register our Routes
+app.use('/api', apiRouter);
 
-  if(password === '123456'){
-    next();
-  }else{
-    res.redirect('/external/error');
-  }
-});
-
-//Rutas
-//adminRouter
-adminRouter.get('/', function(req, res){
-  res.send('Estoy en la pagina principal del admin');
-});
-
-adminRouter.get('/users', function(req, res){
-  console.log('Ya llegue a la vista de usuarios');
-  res.send('Aqui se mostraran los usuarios');
-});
-
-adminRouter.get('/users/:name', function(req, res){
-  //res.send('Hola ' + req.params.name);
-  res.send('Hola ' + req.name);
-});
-
-adminRouter.get('/posts', function(req, res){
-  res.send('Aqui se mostraran los articulos');
-});
+app.listen(port);
+console.log('Neo comes over port ' + port);
 
 
-//externalRouter
-externalRouter.get('/login/:username/:password', function(req, res){
-    res.send('Congratulations ' + req.username + ', you entered!!');
-});
-
-externalRouter.get('/error', function(req, res){
-  res.send('You are not allowed to enter this site. Get out!!')
-});
-
-app.use('/admin', adminRouter);
-app.use('/external', externalRouter);
-
-app.route('/account')
-.get(function(req, res){
-  var message = 'Method GET';
-  console.log(message);
-  res.send(message);
-})
-.post(function(req, res){
-  var message = 'Method POST';
-  console.log(message);
-  res.send(message);
-})
-.put(function(req, res){
-  var message = 'Method PUT';
-  console.log(message);
-  res.send(message);
-})
-.delete(function(req, res){
-  var message = 'Method DELETE';
-  console.log(message);
-  res.send(message);
-})
 
 
-app.set('port', (process.env.PORT || 5000));
-app.listen(app.get('port'));
 
-console.log('Here we go!');
+
+
+
+
+
+//
+// app.get('/', function(request, responsse){
+//   response.sendFile(path.join(__dirname) + '/index.html');
+// });
+//
+// //Middleware
+// //mas generico, para toda la aplicacion
+// adminRouter.use(function (req, res, next){
+//   console.log('------->', req.method, req.url);
+//   next();
+// });
+// //estos trabajan a nivel de parametro
+// adminRouter.param('name', function (req, res, next, name){
+//   console.log('req.name: ', req.name);
+//   console.log('name: ', name);
+//   req.name = 'Mr. robot wa here!';
+//   next();
+// });
+//
+// externalRouter.param('username', function(req, res, next, username){
+//   if(username === 'mrrobot'){
+//     req.username = 'jose';
+//     next();
+//   }else{
+//     res.redirect('/external/error');
+//   }
+// });
+//
+// externalRouter.param('password', function(req, res, next, password){
+//
+//   if(password === '123456'){
+//     next();
+//   }else{
+//     res.redirect('/external/error');
+//   }
+// });
+//
+// //Rutas
+// //adminRouter
+// adminRouter.get('/', function(req, res){
+//   res.send('Estoy en la pagina principal del admin');
+// });
+//
+// adminRouter.get('/users', function(req, res){
+//   console.log('Ya llegue a la vista de usuarios');
+//   res.send('Aqui se mostraran los usuarios');
+// });
+//
+// adminRouter.get('/users/:name', function(req, res){
+//   //res.send('Hola ' + req.params.name);
+//   res.send('Hola ' + req.name);
+// });
+//
+// adminRouter.get('/posts', function(req, res){
+//   res.send('Aqui se mostraran los articulos');
+// });
+//
+//
+// //externalRouter
+// externalRouter.get('/login/:username/:password', function(req, res){
+//     res.send('Congratulations ' + req.username + ', you entered!!');
+// });
+//
+// externalRouter.get('/error', function(req, res){
+//   res.send('You are not allowed to enter this site. Get out!!')
+// });
+//
+// app.use('/admin', adminRouter);
+// app.use('/external', externalRouter);
+//
+// app.route('/account')
+// .get(function(req, res){
+//   var message = 'Method GET';
+//   console.log(message);
+//   res.send(message);
+// })
+// .post(function(req, res){
+//   var message = 'Method POST';
+//   console.log(message);
+//   res.send(message);
+// })
+// .put(function(req, res){
+//   var message = 'Method PUT';
+//   console.log(message);
+//   res.send(message);
+// })
+// .delete(function(req, res){
+//   var message = 'Method DELETE';
+//   console.log(message);
+//   res.send(message);
+// })
+//
+//
+//
+// //app.set('port', (process.env.PORT || 5000));
+// app.listen(app.get('port'));
+
+//console.log('Here we go!');
