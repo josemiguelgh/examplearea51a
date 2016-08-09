73204bb55abe9754a886687eeb1fde4a75df4fdc
Admin <admin@A51-01.local>
2016-08-08 20:18:48 -0500

    reorganize methods

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  'port': process.env.PORT || 5000,
+  'superSecret': 'strangeThingsHappenedInArea51',
+  'database': 'mongodb://localhost/pokemon',
+  'databaseRemote': 'mongodb://admin:admin@ds023105.mlab.com:23105/pokemon'
+}
