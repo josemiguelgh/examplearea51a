936279b9067a3a5db11ef40566709a1acf104c08
Admin <admin@A51-01.local>
2016-08-12 20:25:12 -0500

    change gitignore

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
   'port': process.env.PORT || 5000,
   'superSecret': 'strangeThingsHappenedInArea51',
-  'database': 'mongodb://localhost/pokemon',
-  'databaseRemote': 'mongodb://admin:admin@ds023105.mlab.com:23105/pokemon'
+  // 'database': 'mongodb://localhost/pokemon',
+  'database': 'mongodb://admin:admin@ds023105.mlab.com:23105/pokemon'
 }
